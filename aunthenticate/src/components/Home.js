d1d734b0b07e5fbedb88265ebb6e2ac6ec2aa16a
DesmondM <desmondmpofu@gmail.com>
2021-04-19 21:59:51 +0200

    minor changes

--- aunthenticate/src/components/Home.js
+++ aunthenticate/src/components/Home.js
@@ -4,7 +4,7 @@ import {Form, Button, Card} from "react-bootstrap"
 function Home() {
     return (
         <div>
-           <h1> Welcome Home</h1>
+           <h1> Welcome to Cyber Site</h1>
            <p><Button class="btn btn-warning" onClick={()=>auth.signOut()}>Sign out</Button></p>
         </div>
     )
